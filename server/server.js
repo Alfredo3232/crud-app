7dd19a244fa440ed0ab5699610b1b40fc9ad9a82
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-20 20:21:57 -0600

    add script to start sever and fix express server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const app = express()
-const path = require('path');
 const PORT = 3000
 
-const pathToClient = path.join(__dirname, './index.html')
-
-app.use(express.static(__dirname + "./index.html"));
+app.use(express.static(__dirname + "/../client"))
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + './index.html');
-});
+    res.sendFile(__dirname + '/../client')
+})
 
 app.listen(PORT, () => {
     console.log(`Running at PORT: ${PORT}`)
